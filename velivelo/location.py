5ac583f31e29c7be378672d4a07dde0efd803b51
Eryas <eryasnouri@gmail.com>
2026-04-16 11:32:03 +0200

    Update location.py

--- velivelo/location.py
+++ velivelo/location.py
@@ -3,7 +3,14 @@ from velivelo.location import horsForfait
 
 class TestHorsForfait(unittest.TestCase):
     def testhorsforfaitcas1(self):
-        self.assertEqual(horsForfait(60, 90), "SansDépassement")
+        self.assertEqual(horsForfait(60, 90), "SansDepassement")
 
     def testhorsforfaitcas2(self):
-        self.assertEqual(horsForfait(60, 30), "AvecDépassement")
+        self.assertEqual(horsForfait(60, 30), "AvecDepassement")
+
+    def testhorsforfaitcas3(self):
+        self.assertEqual(horsForfait(241, 380), "Amende")
+
+    def testhorsforfaitcas4(self):
+        with self.assertRaises(ValueError):
+            horsForfait(0, 30)
