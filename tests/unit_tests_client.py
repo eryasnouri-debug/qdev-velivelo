4e6d815ebf490ccdfa1fd66c03ff5ffcc4ac43f0
Eryas <eryasnouri@gmail.com>
2026-04-16 11:47:16 +0200

    Update unit_tests_client.py

--- tests/unit_tests_client.py
+++ tests/unit_tests_client.py
@@ -1,9 +1,19 @@
 import unittest
-from velivelo.client import *
+from velivelo.client import pseudoClientConforme
 
 class TestPseudoClientConforme(unittest.TestCase):
-  def test_exemple(self):
-    self.assertEqual(True, False)
-
+    def testcas1valide(self):
+        self.assertTrue(pseudoClientConforme("Rob96"))
+    def testcas2tropcourt(self):
+        self.assertFalse(pseudoClientConforme("Ro"))
+    def testcas3troplong(self):   
+        self.assertFalse(pseudoClientConforme("Rob96666666"))
+    def testcas4pas_demajuscule(self):
+        self.assertFalse(pseudoClientConforme("rob96"))
+    def testcas5caracterespecial(self):
+        self.assertFalse(pseudoClientConforme("Rob96%"))
+    def testcas6vide(self):
+        with self.assertRaises(ValueError):
+            pseudoClientConforme("")
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
