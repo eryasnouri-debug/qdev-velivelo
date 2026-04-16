1d0c86b73e46480b5cd0b13140a54ca5d6e5adef
Eryas <eryasnouri@gmail.com>
2026-04-16 11:46:52 +0200

    Update unit_tests_location.py

--- tests/unit_tests_location.py
+++ tests/unit_tests_location.py
@@ -1,9 +1,18 @@
 import unittest
-from velivelo.location import *
+from velivelo.location import horsForfait, Verdict
 
 class TestHorsForfait(unittest.TestCase):
-  def test_exemple(self):
-    self.assertEqual(True, False)
+    def testcas1(self):
+        self.assertEqual(horsForfait(60, 90), Verdict.SANS_DEPASSEMENT)
+
+    def testcas2(self):
+        self.assertEqual(horsForfait(60, 30), Verdict.AVEC_DEPASSEMENT)
+
+    def testcas3(self):
+        self.assertEqual(horsForfait(241, 380), Verdict.AMENDE)
+    def testcas4(self):
+        with self.assertRaises(ValueError):
+            horsForfait(0, 30)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
