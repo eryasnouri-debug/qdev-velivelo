ee741b1e3215af6071be3d8288ac5f4154a76a52
Eryas <eryasnouri@gmail.com>
2026-04-16 11:50:49 +0200

    Update client.py

--- velivelo/client.py
+++ velivelo/client.py
@@ -1,2 +1,10 @@
 def pseudoClientConforme(pseudo : str) -> bool:
-  pass
+    if pseudo == "":
+        raise ValueError("pseudo ne peut pas une chaine vide")
+    if not (3 <= len(pseudo) <= 10):
+        return False
+    if not pseudo[0].isupper():
+        return False
+    if not pseudo.isalnum():
+        return False
+    return True
