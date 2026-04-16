a4cc3134e6fd9efa9ee4ae99ad0bb4987f4dce7e
Eryas <eryasnouri@gmail.com>
2026-04-16 11:50:59 +0200

    Update client.py

--- velivelo/client.py
+++ velivelo/client.py
@@ -1,10 +1,16 @@
-def pseudoClientConforme(pseudo : str) -> bool:
-    if pseudo == "":
-        raise ValueError("pseudo ne peut pas une chaine vide")
-    if not (3 <= len(pseudo) <= 10):
-        return False
-    if not pseudo[0].isupper():
-        return False
-    if not pseudo.isalnum():
-        return False
-    return True
+from enum import Enum
+
+class Verdict(Enum):
+    SANS_DEPASSEMENT = 0
+    AVEC_DEPASSEMENT = 1
+    AMENDE = 2
+
+def horsForfait(dureeLocation : int, tempsRestant : int) -> Verdict:
+    if dureeLocation <= 0:
+        raise ValueError("duree de la location doit etre superieure a 0")
+    if dureeLocation > 240:
+        return Verdict.AMENDE
+    if dureeLocation <= tempsRestant:
+        return Verdict.SANS_DEPASSEMENT
+    
+    return Verdict.AVEC_DEPASSEMENT
